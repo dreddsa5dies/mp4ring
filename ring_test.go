ab0121be16c01945d92e3ad766fb44d8a15a29dc
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2022-10-24 20:49:57 +0300

    26% test coverage

--- ring_test.go
+++ ring_test.go
@@ -9,6 +9,13 @@ import (
 	"testing"
 )
 
+// examples of headers
+const (
+	ft = `    ftypisom    isomiso2avc1mp41`
+	mv = `    moov   lmvhd              _�                                                    @                                  �trak   \tkhd                                                                        @        p      mdia    mdhd              _�    U�     -hdlr        vide            VideoHandler    *minf    vmhd               $dinf    dref            url        �stbl   �stsd           �avc1                           p H   H                                          ��   8avcC � (��  g� (�ٲ�� O��-@@@P         (� '   h��D�    stts            stsc            stsz                stco           (mvex    trex                        `
+	r  = `   lmoof    mfhd       4   Ttraf    tfhd   8       �   �        tfdt          �|    trun           t   �   �`
+)
+
 func Test_getHeaderBoxInfo(t *testing.T) {
 	type args struct {
 		data []byte
@@ -19,7 +26,8 @@ func Test_getHeaderBoxInfo(t *testing.T) {
 		wantBoxHeader boxHeader
 		wantErr       bool
 	}{
-		// TODO: Add test cases.
+		{name: "ftyp", args: args{[]byte(ft + mv + r)}, wantBoxHeader: boxHeader{Size: 538976288, FourccType: [4]byte{102, 116, 121, 112}}, wantErr: false},
+		{name: "moov", args: args{[]byte(mv + ft + r)}, wantBoxHeader: boxHeader{Size: 538976288, FourccType: [4]byte{109, 111, 111, 118}}, wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -36,56 +44,36 @@ func Test_getHeaderBoxInfo(t *testing.T) {
 }
 
 func TestNew(t *testing.T) {
-	type args struct {
-		size int
+	var (
+		b   *Buffer
+		err error
+	)
+
+	b, err = New(10)
+
+	if b == nil || err != nil {
+		t.Fatal(`failed to create ring buffer`)
 	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *Buffer
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := New(tt.args.size)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
+
+	b, err = New(-12) //nolint:ineffassign,staticcheck
+
+	if err == nil {
+		t.Fatal(`failed check size buffer`)
 	}
 }
 
 func TestBuffer_Close(t *testing.T) {
-	type fields struct {
-		ftyp     []byte
-		moov     []byte
-		r        *ring.Ring
-		size     int64
-		isClosed bool
-	}
 	tests := []struct {
 		name    string
-		fields  fields
+		buf     *Buffer
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{name: "ok", buf: &Buffer{ftyp: []byte(ft), moov: []byte(mv), r: ring.New(1), size: 100, isClosed: false}, wantErr: false},
+		{name: "nil", buf: nil, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b := &Buffer{
-				ftyp:     tt.fields.ftyp,
-				moov:     tt.fields.moov,
-				r:        tt.fields.r,
-				size:     tt.fields.size,
-				isClosed: tt.fields.isClosed,
-			}
-			if err := b.Close(); (err != nil) != tt.wantErr {
+			if err := tt.buf.Close(); (err != nil) != tt.wantErr {
 				t.Errorf("Buffer.Close() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
